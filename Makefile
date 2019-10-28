3b1367640acd6ad0b10606ece3fc0d758a64c28a
Takashi SASAKI <takashi316@gmail.com>
2019-10-28 11:08:25 +0000

    read lines

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 test:
-	./hexor.py 1 2 3
+	$(MAKE) -C testdata
+	./hexor.py testdata/bin1.md5 testdata/bin2.md5 testdata/bin3.md5
+
--- hexor.py
+++ hexor.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 import sys
 
+class Hexor:
+    def __init__(self):
+        self.hexs = []
+
+    def addFile(self, filename):
+        f = open(filename, mode='r')
+        lines = f.readlines()     
+        for line in lines:
+            fields = line.split()
+            if len(fields) != 2:
+                raise "Each line should have exact two fields."
+            else:
+                hexstring = fields[0]
+                self.hexs.append(hexstring)
+
+    def getHexs(self):
+        return self.hexs
+
 if __name__ == "__main__":
     args = sys.argv
     filenames = args[1:]
-    print(filenames)
+    hexor = Hexor()
+    for filename in filenames:
+        hexor.addFile(filename)
 
-def hexor():
-    pass
+    print(hexor.getHexs())
 
-    
