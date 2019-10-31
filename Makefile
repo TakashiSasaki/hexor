3b635b459d191528512d0c51020d5427fa509516
Takashi SASAKI <takashi316@gmail.com>
2019-10-31 01:09:00 +0000

    add -j option

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 test:
 	$(MAKE) -C testdata
+	./hexor.py -h testdata/bin1.md5 testdata/bin2.md5 testdata/bin3.md5
 	./hexor.py testdata/bin1.md5 testdata/bin2.md5 testdata/bin3.md5
+	./hexor.py -j testdata/bin1.md5 testdata/bin2.md5 testdata/bin3.md5
+
+ntpdate:
+	ntpdate ntp.nict.jp
--- hexor.py
+++ hexor.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-__doc__ = """{f}
-
-Usage:
-        {f} <fname> [-v | --verbose] [-c | --cat <another_file>]
-        {f} -h | --help
-
-Options:
-        -j --json output JSON
-        -h --help 
-""".format(f="hexor")
-
 class Hexor:
     def __init__(self):
-        self.hexs = []
+        self.hexstrings = []
+        self.filenames = []
 
     def addFile(self, filename):
         f = open(filename, mode='r')
@@ -25,24 +15,23 @@ class Hexor:
                 raise "Each line should have exact two fields."
             else:
                 hexstring = fields[0]
-                self.hexs.append(hexstring)
+                filename = fields[1]
+                self.hexstrings.append(hexstring)
+                self.filenames.append(filename)
 
     def getXor(self):
-        if len(self.hexs) == 0:
+        if len(self.hexstrings) == 0:
             raise "At least one file should be given."
-        hexLength = len(self.hexs[0])
+        hexLength = len(self.hexstrings[0])
         accum = "0" * hexLength
-        for hex in self.hexs:
+        for hex in self.hexstrings:
             if len(hex) != hexLength:
                 raise "All hex string should have the same length."
-            accum = xorHexs(accum, hex)
+            accum = xorHexstrings(accum, hex)
         assert len(accum) == hexLength
         return accum
 
-    def getHexs(self):
-        return self.hexs
-
-def xorHexs(xx, yy):
+def xorHexstrings(xx, yy):
     result = ""
     for i in range(len(xx)):
         result += xorChars(xx[i], yy[i]) 
@@ -57,13 +46,31 @@ def xorChars(x, y):
     assert len(z) == 1
     return z
 
+import argparse
+import json
+
 if __name__ == "__main__":
     args = sys.argv
+
+    p = argparse.ArgumentParser()
+    p.add_argument("-j", "--json", action="store_true", help="output as JSON")
+    p.add_argument("file", action="append", nargs="*", help="input file")
+    a = p.parse_args()
+    #print(a.file)
+
     filenames = args[1:]
+    filenames = a.file[0]
     hexor = Hexor()
     for filename in filenames:
         hexor.addFile(filename)
 
-    print(hexor.getHexs())
-    print(hexor.getXor())
+    #print(hexor.getHexs())
+    if a.json == True:
+        print(json.dumps({
+            "xor" : hexor.getXor(),
+            "filenames" : hexor.filenames,
+            "hexstrings" : hexor.hexstrings
+            }, indent=2))
+    else:
+        print(hexor.getXor())
 
